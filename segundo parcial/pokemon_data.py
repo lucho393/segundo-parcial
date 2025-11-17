660d1c56cc779586b7aa9c3e1fdae64341bcc38e
lucho393 <puigluciano393@gmail.com>
2025-11-17 19:57:49 -0300

    Add files via upload

---
+++ segundo parcial/pokemon_data.py
@@ -0,0 +1,106 @@
+# ======================================================
+#   POKEMON_DATA.PY  –  Lista reducida para pruebas
+# ======================================================
+
+pokemons = [
+
+    {
+        "name": "Bulbasaur",
+        "number": 1,
+        "types": ["grass", "poison"],
+        "weakness": ["fire", "ice", "flying"],
+        "mega": False,
+        "gigamax": False
+    },
+
+    {
+        "name": "Ivysaur",
+        "number": 2,
+        "types": ["grass", "poison"],
+        "weakness": ["fire", "ice", "flying"],
+        "mega": False,
+        "gigamax": False
+    },
+
+    {
+        "name": "Charizard",
+        "number": 6,
+        "types": ["fire", "flying"],
+        "weakness": ["water", "rock", "electric"],
+        "mega": True,
+        "gigamax": True
+    },
+
+    {
+        "name": "Pikachu",
+        "number": 25,
+        "types": ["electric"],
+        "weakness": ["ground"],
+        "mega": False,
+        "gigamax": True
+    },
+
+    {
+        "name": "Jolteon",
+        "number": 135,
+        "types": ["electric"],
+        "weakness": ["ground"],
+        "mega": False,
+        "gigamax": False
+    },
+
+    {
+        "name": "Gengar",
+        "number": 94,
+        "types": ["ghost", "poison"],
+        "weakness": ["psychic", "ground", "ghost"],
+        "mega": True,
+        "gigamax": True
+    },
+
+    {
+        "name": "Steelix",
+        "number": 208,
+        "types": ["steel", "ground"],
+        "weakness": ["fire", "water", "fighting", "ground"],
+        "mega": True,
+        "gigamax": False
+    },
+
+    {
+        "name": "Lucario",
+        "number": 448,
+        "types": ["fighting", "steel"],
+        "weakness": ["fire", "ground", "fighting"],
+        "mega": True,
+        "gigamax": False
+    },
+
+    {
+        "name": "Lycanroc",
+        "number": 745,
+        "types": ["rock"],
+        "weakness": ["water", "grass", "fighting", "steel"],
+        "mega": False,
+        "gigamax": False
+    },
+
+    {
+        "name": "Tyrantrum",
+        "number": 697,
+        "types": ["rock", "dragon"],
+        "weakness": ["ice", "dragon", "ground"],
+        "mega": False,
+        "gigamax": False
+    },
+
+    {
+        "name": "Butterfree",
+        "number": 12,
+        "types": ["bug", "flying"],
+        "weakness": ["fire", "rock", "electric", "flying"],
+        "mega": False,
+        "gigamax": True
+    },
+
+]
